a37b9687c6843d850113f67a344f95f710bb8c7e
maib07001 <maib07001@E-PW05C37V>
2024-10-08 13:59:38 +0200

    Add project files.

---
+++ uppgift3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Uppgift_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur longt hoppade Elin, mätt i meter");
+            string Elin = Console.ReadLine();
+            Console.WriteLine("hur longt hoppade Alma, mätt i meter");
+            string alma = Console.ReadLine();
+            int A = int.Parse(Elin);
+            int B = int.Parse(alma);
+            int skillnad = A - B;
+            Console.WriteLine("Elin hoppade " + skillnad + " meter längre än Alma");
+
+
+
+
+
+
+        }
+
+    }
+
+}
